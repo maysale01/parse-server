c4ccf90bc15f3199563a01f91496cf35f6a138db
Alexander Mays <maysale01@gmail.com>
2016-02-01 18:10:43 -0500

    Expose the internal modules
    Signed-off-by: Alexander Mays <maysale01@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 // ParseServer - open-source compatible API Server for Parse apps
 module.exports = {
+  utils: require('./utils'),
+  handlers: require('./handlers'),
+  middlewares: require('./middlewares'),
+  adapters: require('./adapters'),
+  classes: require('./classes'),
   ParseServer: require('./classes').ParseServer
 };
