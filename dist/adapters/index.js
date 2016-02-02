1174320a687282d416072e195b56d9f7f381b8c2
Alexander Mays <maysale01@gmail.com>
2016-02-02 10:32:34 -0500

    updated gitignore
    Signed-off-by: Alexander Mays <maysale01@gmail.com>

--- dist/adapters/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-    ExportAdapter: require('./ExportAdapter'),
-    GridStoreAdapter: require('./GridStoreAdapter'),
-    S3Adapter: require('./S3Adapter')
-};
--- dist/middlewares/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-    allowCrossDomain: require('./allowCrossDomain'),
-    allowMethodOverride: require('./allowMethodOverride'),
-    handleParseErrors: require('./handleParseErrors'),
-    handleParseHeaders: require('./handleParseHeaders')
-};
